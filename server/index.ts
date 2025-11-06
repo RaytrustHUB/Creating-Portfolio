b845a51bd1a414e7f9e694ae11555558b1ef4fbf
RaytrustHUB <ray_truster@yahoo.com>
2025-11-06 18:01:32 -0500

    updates

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
-import { db } from "@db";
+import { db } from "../db/index";
 import { sql } from "drizzle-orm";
 import dotenv from "dotenv";
 
